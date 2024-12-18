5a6c9dbdc93eb010fbfa318848eb625fa2736d07
Ayman ALKHATIB <aemn22384@gmail.com>
2024-12-18 15:20:08 +0100

    create the routes for the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
+import ChatsListPage from "./pages/ChatsListPage";
+import Nav from "./component/Nav";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Nav />}>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/chatsListPage" element={<ChatsListPage />} />
+            <Route path="/chatPage" element={<ChatPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
