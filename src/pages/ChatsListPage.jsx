866c4947f7122eb737f3487fc51605924ee2aeba
Ayman ALKHATIB <aemn22384@gmail.com>
2025-01-09 16:51:41 +0100

    fetch friends list from Supabase and display in ChatsListPage

--- src/pages/ChatsListPage.jsx
+++ src/pages/ChatsListPage.jsx
@@ -1,5 +1,31 @@
-function ChatsListPage() {
-  return <div>ChatsListPage</div>;
+import { useEffect, useState } from "react";
+import { supabase } from "../logic/supabase";
+import ChatList from "../component/chatListPageComponents/ChatList";
+
+function ChatsListPage({ userId }) {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    if (!userId) return;
+    async function fetchFriends() {
+      const { data, error } = await supabase
+        .from("profiles")
+        .select("friends_list")
+        .eq("id", userId);
+
+      if (error) console.error("Error fetching friends", error);
+
+      if (data) setFriends(data[0].friends_list);
+    }
+    fetchFriends();
+  }, [userId]);
+
+  return (
+    <div>
+      <h1>Chats List</h1>
+      <ChatList friends={friends} />
+    </div>
+  );
 }
 
 export default ChatsListPage;
